210657ba96d7c5b7a13d8f779a7baa51683be3e3
algroznykh <groznykh.aleksandr@gmail.com>
2017-04-22 09:05:55 +0300

    add timing url

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 options = [u'Расписание хакатона', u'Список экспертов', u'Задать вопрос организатору', ]
 from telegram import InlineKeyboardButton
 keyboard = [[InlineKeyboardButton(i, callback_data='%s'%i)] for i in options]
+
+TIMING_URL = "https://raw.githubusercontent.com/lw-spb/socialhack_bot/master/timing.txt"
